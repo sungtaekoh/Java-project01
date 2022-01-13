9965abf5282c32784b74f2aff533698859a1b49c
brown <ohbass95@gmail.com>
2022-01-13 11:31:41 +0900

    D02

--- TwentyOneGame/Ex02.java
+++ TwentyOneGame/Ex02.java
@@ -1,62 +1,42 @@
-package TwentyOneGame;
+ package TwentyOneGame;
 
 import java.util.Random;
 import java.util.Scanner;
 
 //21게임
-public class Ex02 {
+public class Ex02 { 
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		Random rd = new Random();
 		String answer = null;
-		int userCard1 = 0;
-		int userCard2 = 0;
-		
-		int comCard1 = 0;
-		int comCard2 = 0;
-		
-		
-		System.out.println("게임을 시작하겠습니까?");
-		System.out.println("Yes!! / y");
-		System.out.println("Noooo!! / n");
+    Card userCards;
+    Card computerCards;
+
+		System.out.println("게임을 시작하겠습니까? \n Yes!! / y \n Noooo!! / n");
+
 		answer = input.next();
 		if(answer.equals("y")) {//게임시작
 			System.out.println("게임을 시작합니다");
 			System.out.println("패를 섞는 중");
-			try {
-				for(int i =3;i>=1;i--) {
-					Thread.sleep(1000);
-					System.out.println(i);
-				}
-			}catch(Exception e) {
-				System.out.println(e);
-			}
-			userCard1 = rd.nextInt(9)+1;
-			userCard2 = rd.nextInt(9)+1;
-			System.out.println("당신의 첫번째 카드 : "+userCard1);
-			System.out.println("당신의 두번째 카드 : "+userCard2);
-			int userSum = userCard1+userCard2;
+			countDown();
+      int randomNumber = getRandomNumber();
+      userCards = new Card(getRandomNumber(), getRandomNumber());
+
+			System.out.println("당신의 첫번째 카드 : "+userCards.card1);
+			System.out.println("당신의 두번째 카드 : "+userCards.card2);
+			int userSum = userCards.card1+userCards.card2;
 			System.out.println("합계 : "+userSum);
-			comCard1 = rd.nextInt(9)+1;
-			comCard2 = rd.nextInt(9)+1;
-			try {
-				for(int i =3;i>=1;i--) {
-					Thread.sleep(1000);
-					System.out.println(i);
-				}
-			}catch(Exception e) {
-				
-				System.out.println(e);
-			}
-			System.out.println("상대방의 첫번쨰 카드 : "+comCard1);
-			System.out.println("상대방의 첫번쨰 카드 : "+comCard2);
-			int comSum = comCard1+comCard2;
+      computerCards = new Card(getRandomNumber(), getRandomNumber());
+			countDown();
+			System.out.println("상대방의 첫번쨰 카드 : "+computerCards.card1);
+			System.out.println("상대방의 첫번쨰 카드 : "+computerCards.card2);
+			int comSum = computerCards.card1+computerCards.card2;
 			System.out.println("합계 : "+comSum);
-			if(userSum>comSum) {
+			if(userSum > comSum) {
 				System.out.println("당신의 승리 !!");
 			}else if(userSum<comSum) {
 				System.out.println("당신의 패배!!");
-			}else {
+			} else {
 				System.out.println("무승부~!!!");
 			}
 		}else if(answer.equals("n")) {//게임 종료
@@ -65,4 +45,36 @@ public class Ex02 {
 			System.out.println("잘 못 입력하셨습니다");
 		}
 	}
+
+
+   static int getRandomNumber() {
+    Random rd = new Random();
+    return  rd.nextInt(20)+1;
+  }
+
+  static void countDown() {
+    try {
+        for(int i =3;i>=1;i--) {
+          Thread.sleep(1000);
+          System.out.println(i);
+        }
+      }catch(Exception e) {
+        
+        System.out.println(e);
+      }
+  }
+
+
+  
+
+}
+class Card {
+  public int card1;
+  public int card2;
+
+  public Card(int card1, int card2) { 
+    this.card1 = card1;
+    this.card2 = card2;
+
+  }
 }
